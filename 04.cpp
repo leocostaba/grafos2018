9956b60178a4b66d9625cb5391687afe5964d71c
leocostaba <leocostaba1@gmail.com>
2018-12-23 19:51:01 -0300

    adiciona melhorias

--- 04.cpp
+++ 04.cpp
@@ -1,8 +1,11 @@
+#include <bits/stdc++.h>
 #include <stdio.h> 
 #include <iostream>
 #include <list>
 #include <vector>
 #include <queue>
+#include <set>
+
 #define INFINITO 10000000
 
 using namespace std;
@@ -10,22 +13,13 @@ using namespace std;
 class Grafo
 {
 private:
-	int V; // número de vértices
-
-	// ponteiro para um array contendo as listas de adjacências
+	int V; 
 	list<pair<int, int> > * adj;
 
 public:
 
-	// construtor
-	Grafo(int V)
-	{
-		this->V = V; // atribui o número de vértices
-
-		/*
-			cria as listas onde cada lista é uma lista de pairs
-			onde cada pair é formado pelo vértice destino e o custo
-		*/
+	Grafo(int V){
+		this->V = V;
 		adj = new list<pair<int, int> >[V];
 	}
 
@@ -35,87 +29,55 @@ public:
 	
 	int criarNovasArestas(vector<int> *impares);
 
-	// adiciona uma aresta ao grafo de v1 à v2
-	void addAresta(int v1, int v2, int custo)
-	{
+	void addAresta(int v1, int v2, int custo){
 		adj[v1].push_back(make_pair(v2, custo));
         adj[v2].push_back(make_pair(v1, custo));
 	}
 
-	// algoritmo de Dijkstra
-	int dijkstra(int orig, int dest)
-	{
-		// vetor de distâncias
+	int dijkstra(int orig, int dest){
 		int dist[V];
-
-		/*
-		   vetor de visitados serve para caso o vértice já tenha sido
-		   expandido (visitado), não expandir mais
-		*/
 		int visitados[V];
 
-		// fila de prioridades de pair (distancia, vértice)
 		priority_queue < pair<int, int>,
 					   vector<pair<int, int> >, greater<pair<int, int> > > pq;
 
-		// inicia o vetor de distâncias e visitados
-		for(int i = 0; i < V; i++)
-		{
+		for(int i = 0; i < V; i++){
 			dist[i] = INFINITO;
 			visitados[i] = false;
 		}
 
-		// a distância de orig para orig é 0
 		dist[orig] = 0;
 
-		// insere na fila
 		pq.push(make_pair(dist[orig], orig));
 
-		// loop do algoritmo
-		while(!pq.empty())
-		{
-			pair<int, int> p = pq.top(); // extrai o pair do topo
-			int u = p.second; // obtém o vértice do pair
-			pq.pop(); // remove da fila
 
-			// verifica se o vértice não foi expandido
-			if(visitados[u] == false)
-			{
-				// marca como visitado
+		while(!pq.empty()){
+			pair<int, int> p = pq.top();
+			int u = p.second; 
+			pq.pop(); 
+			if(visitados[u] == false){
 				visitados[u] = true;
-
 				list<pair<int, int> >::iterator it;
-
-				// percorre os vértices "v" adjacentes de "u"
-				for(it = adj[u].begin(); it != adj[u].end(); it++)
-				{
-					// obtém o vértice adjacente e o custo da aresta
+				for(it = adj[u].begin(); it != adj[u].end(); it++){
 					int v = it->first;
 					int custo_aresta = it->second;
-
-					// relaxamento (u, v)
 					if(dist[v] > (dist[u] + custo_aresta))
 					{
-						// atualiza a distância de "v" e insere na fila
 						dist[v] = dist[u] + custo_aresta;
 						pq.push(make_pair(dist[v], v));
 					}
 				}
 			}
 		}
-
-		// retorna a distância mínima até o destino
 		return dist[dest];
 	}
 };
 
-int Grafo::verticeDesconexo(int index) 
-{ 
+int Grafo::verticeDesconexo(int index) { 
     return adj[index].size();
 } 
 
-bool Grafo::impar(int index) 
-{ 
+bool Grafo::impar(int index){ 
     if(adj[index].size() % 2 == 0){
         return false;
     }
@@ -123,30 +85,33 @@ bool Grafo::impar(int index)
 } 
 
 int Grafo::criarNovasArestas(vector<int> *impares){
-    int caminho, menorCaminhoEntreImpares, menorU, menorV;
-
+    int caminho, menorCaminhoEntreImpares=0, menorU, menorV;
+	priority_queue<pair<int, pair<int,int> > >PQ;
     if(impares->size() == 2){
         return dijkstra((*impares)[0], (*impares)[1]);
     }else{
-        menorCaminhoEntreImpares = INFINITO;
         for(int i = 0;  i < impares->size(); i++){
             for(int j = i+1;  j < impares->size(); j++){
                 caminho = dijkstra((*impares)[i], (*impares)[j]);
-                if(caminho < menorCaminhoEntreImpares){
-                    menorCaminhoEntreImpares = caminho;
-                    menorU = i;
-                    menorV = j;
-                }
-            }
+				PQ.push(make_pair(caminho, make_pair(i,j)));
         }
-        impares->erase(impares->begin() + menorU - 1);
-        impares->erase(impares->begin() + menorV - 2);
-        return menorCaminhoEntreImpares + criarNovasArestas(impares);
+    }
+	set<int>S;
+	while (!PQ.empty()) {
+		auto i = PQ.top();
+		PQ.pop();
+		if (S.find(i.second.first) != S.end() && S.find(i.second.first) != S.end()) {
+			menorCaminhoEntreImpares+=i.first;
+			S.insert(i.second.first);
+			S.insert(i.second.second);
+		}
+	}
+    return menorCaminhoEntreImpares;
     }
 }
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
     int PesoArestasIniciais = 0;
     int u,v,weight;
     int CaminhosTuristicos, pontosTuristicos;
@@ -165,14 +130,14 @@ int main(int argc, char *argv[])
 
     for(int i = 0; i < pontosTuristicos; i++){
         if(g.verticeDesconexo(i) == 0){
-            cout << "IMPOSÍVEL VISITAR TODOS OS CAMINHOS E PONTOS TURISTICOS, OS PONTOS TURISTICOS SÃO DESCONEXOS! ";
+            cout << "IMPOSIVEL VISITAR TODOS OS CAMINHOS E PONTOS TURISTICOS, OS PONTOS TURISTICOS S�O DESCONEXOS! ";
 			exit(0);
         }
         if(g.impar(i)){
             impares->push_back(i);
         }
     }
-    cout << "Serão gastos ";
+    cout << "Serao gastos ";
     if(impares->size() == 0){
         cout << PesoArestasIniciais;
 
